ef59fc3b26ae31389c32f7dfa612bf527d6c7dcb
andreik <gs1994@gmail.com>
2018-11-02 19:58:25 +0300

    gdmn-recordset: test based on some real data

--- packages/gdmn-recordset/test/recordset.test.ts
+++ packages/gdmn-recordset/test/recordset.test.ts
@@ -1,22 +1,128 @@
-import { RecordSet } from '../src';
+import { RecordSet, TFieldType } from '../src';
 import { List } from 'immutable';
+import { nbrbCurrencies, INBRBCurrency } from './nbrbcurrencies';
 
 describe('recordset', () => {
 
   it('test', () => {
 
+    const fieldDefs = [
+      {
+        fieldName: 'Cur_Abbreviation',
+        dataType: TFieldType.String,
+        caption: 'Буквенный код',
+        required: true,
+        size: 3
+      },
+      {
+        fieldName: 'Cur_Name',
+        dataType: TFieldType.String,
+        caption: 'Наименование',
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_ID',
+        dataType: TFieldType.Integer,
+        caption: 'Внутренний код',
+        required: true
+      },
+      {
+        fieldName: 'Cur_ParentID',
+        dataType: TFieldType.Integer,
+        caption: 'Внутренний код для связи',
+        required: true
+      },
+      {
+        fieldName: 'Cur_Code',
+        dataType: TFieldType.String,
+        caption: 'Цифровой код',
+        required: true,
+        size: 3
+      },
+      {
+        fieldName: 'Cur_Name_Bel',
+        dataType: TFieldType.String,
+        caption: 'Назва',
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_Name_Eng',
+        dataType: TFieldType.String,
+        caption: 'Name',
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_QuotName',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_QuotName_Bel',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_QuotName_Eng',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_NameMulti',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_Name_BelMulti',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_Name_EngMulti',
+        dataType: TFieldType.String,
+        required: true,
+        size: 60
+      },
+      {
+        fieldName: 'Cur_Scale',
+        dataType: TFieldType.Integer,
+        caption: 'Количество единиц',
+        required: true
+      },
+      {
+        fieldName: 'Cur_Periodicity',
+        dataType: TFieldType.Integer,
+        caption: 'Периодичность выставления курса',
+        required: true
+      },
+      {
+        fieldName: 'Cur_DateStart',
+        dataType: TFieldType.Date,
+        caption: 'Дата включения в перечень валют',
+        required: true
+      },
+      {
+        fieldName: 'Cur_DateEnd',
+        dataType: TFieldType.Date,
+        caption: 'Дата исключения из перечня валют',
+        required: true
+      },
+    ];
+
     const rs = new RecordSet(
       'test',
-      [],
-      List([]),
-      0,
-      [],
-      false,
-      [],
-      undefined
+      fieldDefs,
+      List<INBRBCurrency>(nbrbCurrencies as any)
     );
 
-    expect(rs.data.size).toEqual(0);
+    expect(rs.data.size).toEqual(224);
   });
 
 });
