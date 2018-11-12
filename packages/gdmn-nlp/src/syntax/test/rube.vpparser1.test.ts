da800f87556c930f5c11ccdaa6d46d5edd26dcc8
Elena Buraya <lenusik343buraya@gmail.com>
2018-11-12 12:02:34 +0300

    gdmn-nlp: some tests

--- packages/gdmn-nlp/src/syntax/test/rube.vpparser1.test.ts
+++ packages/gdmn-nlp/src/syntax/test/rube.vpparser1.test.ts
@@ -22,6 +22,22 @@ describe("vpparser1", () => {
     expect((pp!.items[1] as RusWord).word).toEqual('минска');
   });
 
+  test("покажи все организации из минска и минска", () => {
+    const result = parsePhrase('покажи все организации из минска и минска');
+    const vp = result.phrase;
+    expect(vp).toBeDefined();
+    const verb = vp!.items[0] as RusVerb;
+    expect(verb).toBeDefined();
+    expect(verb.word).toEqual('покажи');
+    const np = vp!.items[1] as RusNP;
+    const anp = np!.items[0] as RusANP;
+    const pp = np!.items[1] as RusPP;
+    expect((anp!.items[0] as RusWord).word).toEqual('все');
+    expect((anp!.items[1] as RusWord).word).toEqual('организации');
+    expect((pp!.items[0] as RusWord).word).toEqual('из');
+    expect((pp!.items[1] as RusWord).word).toEqual('минска');
+  });
+
   test("покажи минские организации", () => {
     const result = parsePhrase('покажи минские организации');
     const vp = result.phrase;
--- packages/gdmn-nlp/src/syntax/test/tokenizer.test.ts
+++ packages/gdmn-nlp/src/syntax/test/tokenizer.test.ts
@@ -9,5 +9,14 @@ test('tokenizer', () => {
   
   const tokens3 = tokenize('отсортируй по названию');
   expect(tokens3.length).toEqual(5);
+
+  const tokens4 = tokenize('отсортируй по названию и убыванию');
+  expect(tokens4.length).toEqual(9);
+
+  const tokens5 = tokenize('отсортируй по названию, по возрастанию');
+  expect(tokens5.length).toEqual(10);
+
+  const tokens6 = tokenize('отсортируй по названию, затем по адресу');
+  expect(tokens6.length).toEqual(12);
 });
 
