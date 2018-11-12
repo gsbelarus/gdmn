dc848bbee2ee50f82437696660613cec0c6779e6
andreik <gs1994@gmail.com>
2018-11-12 19:50:47 +0300

    gdmn-nlp-demo: to be contd...

--- src/gdmn-nlp-demo/src/App.tsx
+++ src/gdmn-nlp-demo/src/App.tsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { Route, Link, HashRouter } from 'react-router-dom';
+import Morphology from './morphology/Morphology';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          gdmn-nlp-demo
-        </header>
-        <DefaultButton>
-          I am a button.
-        </DefaultButton>
+        <HashRouter>
+          <div>
+            <ul>
+              <li>
+                <Link to="/morphology">Morphology</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route path="/morphology" component={Morphology} />
+          </div>
+        </HashRouter>
       </div>
     );
   }
---
+++ src/gdmn-nlp-demo/src/morphology/Morphology.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Morphology extends Component {
+  render() {
+    return (
+      <div className="Morphology">
+        morphology...
+      </div>
+    );
+  }
+}
+
+export default Morphology;
