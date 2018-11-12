0aaeb511d1f7caa20d38e72659abff1defb06010
andreik <gs1994@gmail.com>
2018-11-12 19:36:36 +0300

    gdmn-nlp-demo: office-ui-fabric-react

--- src/gdmn-nlp-demo/src/App.tsx
+++ src/gdmn-nlp-demo/src/App.tsx
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          gdmn-nlp-demo
         </header>
+        <DefaultButton>
+          I am a button.
+        </DefaultButton>
       </div>
     );
   }
