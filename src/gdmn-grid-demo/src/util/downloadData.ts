780e895a311ded274d9f630d70488c6a8a39ff4e
sywka <lomashko.yuri@gmail.com>
2018-11-20 17:10:24 +0300

    gdmn-grid-demo: add local script for download data

--- src/gdmn-grid-demo/src/util/downloadData.ts
+++ src/gdmn-grid-demo/src/util/downloadData.ts
@@ -4,17 +4,17 @@
  * import подключаем сразу в программу.
  */
 
-import fs from 'fs';
-import fetch from 'node-fetch';
-import { NBRBCurrencies, NBRBRates } from '../app/types';
+import fs from "fs";
+import fetch from "node-fetch";
+import {NBRBCurrencies, NBRBRates} from "../app/types";
 
-const urlNBRBCurrencies = 'http://www.nbrb.by/API/ExRates/Currencies';
-const urlNBRBRates = 'http://www.nbrb.by/API/ExRates/Rates';
+const urlNBRBCurrencies = "http://www.nbrb.by/API/ExRates/Currencies";
+const urlNBRBRates = "http://www.nbrb.by/API/ExRates/Rates";
 
 fetch(urlNBRBCurrencies)
-  .then( res => res.text() )
-  .then( res => JSON.parse(res) as NBRBCurrencies )
-  .then( res => fs.writeFileSync('./nbrbcurrencies.json', JSON.stringify(res, undefined, 2)) );
+  .then(res => res.text())
+  .then(res => JSON.parse(res) as NBRBCurrencies)
+  .then(res => fs.writeFileSync("./src/util/nbrbcurrencies.json", JSON.stringify(res, undefined, 2)));
 
 const startDate = new Date(2014, 1, 1);
 const endDate = new Date(2018, 11, 1);
@@ -22,14 +22,14 @@ const endDate = new Date(2018, 11, 1);
 const downloadRates = (d: Date, endDate: Date, rates: NBRBRates): Promise<NBRBRates> => {
   if (d < endDate) {
     return fetch(`${urlNBRBRates}?Periodicity=0&onDate=${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`)
-      .then( res => res.text() )
-      .then( res => JSON.parse(res) as NBRBRates )
-      .then( res => downloadRates(new Date(d.setDate(d.getDate() + 1)), endDate, rates.concat(res)) );
+      .then(res => res.text())
+      .then(res => JSON.parse(res) as NBRBRates)
+      .then(res => downloadRates(new Date(d.setDate(d.getDate() + 1)), endDate, rates.concat(res)));
   } else {
     return Promise.resolve(rates);
   }
-}
+};
 
 downloadRates(startDate, endDate, [])
-  .then( res => fs.writeFileSync('./nbrbrates.json', JSON.stringify(res, undefined, 2)) )
-  .catch( console.log );
+  .then(res => fs.writeFileSync("./src/util/nbrbrates.json", JSON.stringify(res, undefined, 2)))
+  .catch(console.log);
--- src/gdmn-grid-demo/src/util/readme.md
+++ src/gdmn-grid-demo/src/util/readme.md
@@ -1,6 +1,9 @@
 Запустите скрипт **downloadData.ts**:
 
 ```ts-node downloadData.ts```
+или
+```yarn download-data``` из `./gdmn-grid-demo`
+
 
 Скрипт сформирует файлы с демонстрационными данными **nbrbcurrencies.json** и **nbrbrates.json**. 
 
