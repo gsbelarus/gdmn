77f0253a9ac5ecad1da01eb2c2d3b0bc04f55420
Lomashko Yuri <lomashko.yuri@gmail.com>
2018-11-29 18:46:45 +0300

    gdmn-orm: fix test

--- packages/gdmn-orm/test/ermodel.test.ts
+++ packages/gdmn-orm/test/ermodel.test.ts
@@ -5,47 +5,12 @@ describe("ERModel", async () => {
 
   it("serialize/deserialize", async () => {
     const erModel = new ERModel();
-    await erModel.init();
-
-    const connection = await erModel.createConnection();
-    try {
-      const transaction = await erModel.startTransaction(connection);
-
-      try {
-        const entity = await erModel.create(new Entity({
-          name: "TEST", lName: {en: {name: "Test"}}
-        }), connection, transaction);
-        const testAttr = await entity.create(new StringAttribute({
-          name: "TEST_FIELD", lName: {en: {name: "Test field"}}
-        }), connection, transaction);
-        await entity.addAttrUnique([testAttr], connection, transaction);
-      } finally {
-        if (!transaction.finished) {
-          await transaction.commit();
-        }
-      }
-    } finally {
-      if (connection.connected) {
-        await connection.disconnect();
-      }
-    }
-
-    const serialized = erModel.serialize();
-
-    const erModel2 = deserializeERModel(serialized);
-
-    expect(erModel).toEqual(erModel2);
-    expect(erModel2.entities.TEST.isTree).toBeFalsy();
-  });
-
-  it("serialize/deserialize (old)", async () => {
-    const erModel = new ERModel();
 
     const entity = erModel.add(new Entity({name: "Test", lName: {en: {name: "Test"}}}));
     const testAttr = entity.add(new StringAttribute({
       name: "TEST_FIELD", lName: {en: {name: "Test field"}}
     }));
-    await entity.addUnique([testAttr]);
+    await entity.add([testAttr]);
 
     const serialized = erModel.serialize();
 
