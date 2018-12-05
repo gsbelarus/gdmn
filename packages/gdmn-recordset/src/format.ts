16b47f2334b619c9f05394fc0a645496cf25f2dc
andreik <gs1994@gmail.com>
2018-12-05 12:14:44 +0300

    gdmn-recordset: optimizations

--- packages/gdmn-recordset/src/format.ts
+++ packages/gdmn-recordset/src/format.ts
@@ -13,64 +13,76 @@ export type FormatNumber = (n: number, format?: INumberFormat) => string;
 
 export const formatNumber: FormatNumber = (n: number, format?: INumberFormat): string => {
 
-  if (format) {    
-    const { minDecDigits, maxDecDigits, useGrouping, groupSeparator, decSeparator, minIntDigits, currSign, currSignPlaceBefore } = format; 
-    let res = n.toString();
-    let m = res.split('.');
-    if (minDecDigits !== undefined || maxDecDigits !== undefined) {   
-      let c = m.length > 1 ? m[1].length : 0;
-
-      if (minDecDigits) {
-        c = Math.max(c, minDecDigits);       
-      }
-
-      if (maxDecDigits) {    
-        c = Math.min(c, maxDecDigits);
-      }      
-
-      res = n.toFixed(c);
-    } 
-
-    if (minIntDigits !== undefined) {
-      for (let i = minIntDigits - m[0].length; i > 0; i--) {
-        res = '0' + res;
-      }
-    }
-
-    if (useGrouping) {
-      m = res.split('.');
-      if (m[0][0] === '-') {
-        res = m[0].substr(1, m[0].length)
-      } else
-        res = m[0];
-      let j = 1;
-      for (let i = res.length; i > 3; i = i-3) {
-        res = res.substr(0, i - 3) + (groupSeparator == undefined ? ' ' : groupSeparator) + res.substr(i - 3, j * 3 + j);
-        j = j + 1;
-      }   
-      if (m.length > 1) {
-        res = res + '.' + m[1];
-      }   
-      if (m[0][0] === '-') {
-        res = '-' + res
-      } 
-    }
-
-    if (decSeparator) {
-      res = res.replace('.', decSeparator);
-    }
-    
-    if (currSign != undefined) {
-      if (currSignPlaceBefore) {
-        res = currSign + ' ' + res;
-      } else {
-        res = res + ' ' + currSign;
-      }
-    }
-    return res;  
+  if (!format) {
+    return n.toString();
   }
-  
-  return n.toString();
+
+  const { minDecDigits, maxDecDigits, useGrouping, groupSeparator, decSeparator, minIntDigits, currSign, currSignPlaceBefore } = format;
+  let res = n.toString();
+  let m = res.split('.');
+
+  if (minDecDigits !== undefined || maxDecDigits !== undefined) {
+    let decDigits = m[1] ? m[1].length : 0;
+
+    if (minDecDigits) {
+      decDigits = Math.max(decDigits, minDecDigits);
+    }
+
+    if (maxDecDigits) {
+      decDigits = Math.min(decDigits, maxDecDigits);
+    }
+
+    res = n.toFixed(decDigits);
+  }
+
+  if (minIntDigits) {
+    for (let i = minIntDigits - m[0].length; i > 0; i--) {
+      res = '0' + res;
+    }
+  }
+
+  if (useGrouping) {
+    let b = res.lastIndexOf('.');
+    let e = res.length;
+
+    if (b === -1) {
+      b = res.length - 1;
+    } else {
+      b--;
+    }
+
+    const parts: string[] = [];
+    let g = 1;
+
+    while (b > 0 && res[b - 1] !== '-' && res[b - 1] !== '+') {
+      if (g === 3) {
+        parts.push(res.slice(b, e));
+        e = b;
+        g = 1;
+      } else {
+        g++;
+      }
+      b--;
+    }
+
+    parts.push(res.slice(0, e));
+
+    res = parts.reverse().join(groupSeparator || ' ')
+  }
+
+  if (decSeparator) {
+    res = res.replace('.', decSeparator);
+  }
+
+  if (currSign) {
+    if (currSignPlaceBefore) {
+      res = currSign + res;
+    } else {
+      res = res + currSign;
+    }
+  }
+
+  return res;
 };
 
 export const globalNumberFormat: INumberFormat = {
@@ -79,8 +91,8 @@ export const globalNumberFormat: INumberFormat = {
 
 export interface IDateOptions {
     weekday?: 'narrow' | 'short' | 'long';
-    year?: 'numeric' | '2-digit'; 
-    month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long'; 
+    year?: 'numeric' | '2-digit';
+    month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long';
     day?: 'numeric' | '2-digit';
     hour?: 'numeric' | '2-digit';
     minute?: 'numeric' | '2-digit';
@@ -95,10 +107,10 @@ export interface IDateFormat {
 export type FormatDate = (d: Date, format?: IDateFormat) => string;
 
 export const formatDate: FormatDate = (d: Date, format?: IDateFormat): string => {
-  if (format) {    
-    let dateTimeFormat = new Intl.DateTimeFormat(format.locales, format.options); 
+  if (format) {
+    let dateTimeFormat = new Intl.DateTimeFormat(format.locales, format.options);
      return dateTimeFormat.format(d);
-    }  
+    }
   return d.toString();
 };
 
--- packages/gdmn-recordset/test/format.test.ts
+++ packages/gdmn-recordset/test/format.test.ts
@@ -8,43 +8,51 @@ describe('format', () => {
     expect(formatNumber(1,       { minDecDigits: 2 })).toEqual('1.00');
     expect(formatNumber(1.001,   { minDecDigits: 2 })).toEqual('1.001');
 
+    expect(formatNumber(1,       { })).toEqual('1');
+    expect(formatNumber(1,       { minIntDigits: 1 })).toEqual('1');
+    expect(formatNumber(1,       { minIntDigits: 4 })).toEqual('0001');
+    expect(formatNumber(1234,    { minIntDigits: 4 })).toEqual('1234');
+    expect(formatNumber(12345,   { minIntDigits: 4 })).toEqual('12345');
+
     expect(formatNumber(1.01,    { maxDecDigits: 2 })).toEqual('1.01');
     expect(formatNumber(1,       { maxDecDigits: 2 })).toEqual('1');
     expect(formatNumber(1.001,   { maxDecDigits: 2 })).toEqual('1.00');
 
     expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4 })).toEqual('1.01');
+    expect(formatNumber(1,       { minDecDigits: 2, maxDecDigits: 4 })).toEqual('1.00');
     expect(formatNumber(1.001,   { minDecDigits: 2, maxDecDigits: 4 })).toEqual('1.001');
     expect(formatNumber(1.0001,  { minDecDigits: 2, maxDecDigits: 4 })).toEqual('1.0001');
     expect(formatNumber(1.00001, { minDecDigits: 2, maxDecDigits: 4 })).toEqual('1.0000');
-   
+
     expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 5, decSeparator: ',' })).toEqual('00001,01');
-    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: 'BYN' })).toEqual('1.01 BYN');
-    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true })).toEqual('$ 1.01');    
-    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: 'E', currSignPlaceBefore: false })).toEqual('1.01 E');        
-    
-    expect(formatNumber(123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: true })).toEqual('123 456 789.0000');
-    expect(formatNumber(123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: false, groupSeparator: ' ' })).toEqual('123456789.0000');
-    expect(formatNumber(123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ' ' })).toEqual('123 456 789.0000');
-    expect(formatNumber(123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ',' })).toEqual('123,456,789.0000');
-    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 5, decSeparator: ',', useGrouping: true, groupSeparator: ' ' })).toEqual('00 001,01');
-    expect(formatNumber(12345678912585.01, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: 'BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('12 345 678 912 585.01 BYN');
-    expect(formatNumber(1.1135, { minDecDigits: 2, maxDecDigits: 2, minIntDigits: 1, currSign: 'BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('1.11 BYN');    
-    expect(formatNumber(0.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: false, groupSeparator: ' ' })).toEqual('$ 0.0111');    
-    expect(formatNumber(0.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 0.0111');        
-    expect(formatNumber(1234560.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: false, groupSeparator: ' ' })).toEqual('$ 1234560.0111');        
-    expect(formatNumber(1234560.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ',' })).toEqual('$ 1,234,560.0111');        
-    expect(formatNumber(234560.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 234 560.0111');            
+    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: ' BYN' })).toEqual('1.01 BYN');
+    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true })).toEqual('$ 1.01');
+    expect(formatNumber(1.01,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: ' E', currSignPlaceBefore: false })).toEqual('1.01 E');
 
-    expect(formatNumber(234560, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 234 560.00');            
+    expect(formatNumber(123456789.00001,   { minDecDigits: 2, maxDecDigits: 4, useGrouping: true })).toEqual('123 456 789.0000');
+    expect(formatNumber(123456789.00001,   { minDecDigits: 2, maxDecDigits: 4, useGrouping: false, groupSeparator: ' ' })).toEqual('123456789.0000');
+    expect(formatNumber(123456789.00001,   { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ' ' })).toEqual('123 456 789.0000');
+    expect(formatNumber(123456789.00001,   { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ',' })).toEqual('123,456,789.0000');
+    expect(formatNumber(1.01,              { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 5, decSeparator: ',', useGrouping: true, groupSeparator: ' ' })).toEqual('00 001,01');
+    expect(formatNumber(12345678912585.01, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: ' BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('12 345 678 912 585.01 BYN');
+    expect(formatNumber(1.1135,            { minDecDigits: 2, maxDecDigits: 2, minIntDigits: 1, currSign: ' BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('1.11 BYN');
+    expect(formatNumber(0.01111,           { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: false, groupSeparator: ' ' })).toEqual('$ 0.0111');
+    expect(formatNumber(0.01111,           { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 0.0111');
+    expect(formatNumber(1234560.01111,     { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: false, groupSeparator: ' ' })).toEqual('$ 1234560.0111');
+    expect(formatNumber(1234560.01111,     { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ',' })).toEqual('$ 1,234,560.0111');
+    expect(formatNumber(234560.01111,      { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 234 560.0111');
 
-    expect(formatNumber(-123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: true })).toEqual('-123 456 789.0000');
-    expect(formatNumber(-123456789.00001, { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ',' })).toEqual('-123,456,789.0000');
-    expect(formatNumber(-12345678912585.01, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: 'BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('-12 345 678 912 585.01 BYN');
-    expect(formatNumber(-0.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ -0.0111');        
-    expect(formatNumber(-1234560.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ',' })).toEqual('$ -1,234,560.0111');        
-    expect(formatNumber(-234560.01111, { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ -234 560.0111');            
+    expect(formatNumber(234560,            { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ 234 560.00');
 
+    expect(formatNumber(-123456789.00001,  { minDecDigits: 2, maxDecDigits: 4, useGrouping: true })).toEqual('-123 456 789.0000');
+    expect(formatNumber(-123456789.00001,  { minDecDigits: 2, maxDecDigits: 4, useGrouping: true, groupSeparator: ',' })).toEqual('-123,456,789.0000');
+    expect(formatNumber(-12345678912585.01,{ minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: ' BYN', useGrouping: true, groupSeparator: ' ' })).toEqual('-12 345 678 912 585.01 BYN');
+    expect(formatNumber(-0.01111,          { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ -0.0111');
+    expect(formatNumber(-1234560.01111,    { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ',' })).toEqual('$ -1,234,560.0111');
+    expect(formatNumber(-234560.01111,     { minDecDigits: 2, maxDecDigits: 4, minIntDigits: 1, currSign: '$ ', currSignPlaceBefore: true, useGrouping: true, groupSeparator: ' ' })).toEqual('$ -234 560.0111');
+  });
 
+  it('format date', () => {
     const d = new Date(2012, 5);
     expect(formatDate(d, { locales: 'en-US', options: {year: 'numeric', month: 'long', day: 'numeric'} })).toEqual('June 1, 2012');
   });
