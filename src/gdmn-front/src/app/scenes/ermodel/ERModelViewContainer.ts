9af286c65c319fb4391aa0d59db065260f3f88af
andreik <gs1994@gmail.com>
2018-12-13 14:05:24 +0300

    gdmn-front: refactoring

--- src/gdmn-front/src/app/scenes/ermodel/ERModelViewContainer.ts
+++ src/gdmn-front/src/app/scenes/ermodel/ERModelViewContainer.ts
@@ -2,10 +2,11 @@ import { ERModelView } from './ERModelView';
 import { IState } from '@src/app/store/reducer';
 import { connect } from 'react-redux';
 import { ERModel } from 'gdmn-orm';
-import { RecordSet, TFieldType, createRecordSet, RecordSetAction, SortFields, sortRecordSet, selectRow, setAllRowsSelected, setCurrentRow, toggleGroup } from 'gdmn-recordset';
-import { createGrid, GridAction, cancelSortDialog, applySortDialog, resizeColumn, columnMove, setCursorCol, GDMNGrid } from 'gdmn-grid';
+import { RecordSet, TFieldType, createRecordSet, RecordSetAction } from 'gdmn-recordset';
+import { createGrid, GridAction } from 'gdmn-grid';
 import { List } from 'immutable';
 import { ThunkDispatch } from 'redux-thunk';
+import { connectDataViewDispatch } from '../components/connectDataView';
 
 export const ERModelViewContainer = connect(
   (state: IState) => ({
@@ -24,97 +25,7 @@ export const ERModelViewContainer = connect(
   }),
 
   (dispatch: ThunkDispatch<IState, never, GridAction | RecordSetAction>) => ({
-
-    onCancelSortDialog:
-      (gridName: string) => dispatch(cancelSortDialog({ name: gridName })),
-
-    onApplySortDialog:
-      (rs: RecordSet, gridName: string, sortFields: SortFields, gridRef?: GDMNGrid) => dispatch(
-        (dispatch: ThunkDispatch<IState, never, GridAction | RecordSetAction>, getState: () => IState) => {
-          dispatch(applySortDialog({ name: gridName, sortFields }));
-          dispatch(sortRecordSet({ name: rs.name, sortFields }));
-          if (gridRef) {
-            gridRef.scrollIntoView(getState().recordSet[rs.name].currentRow);
-          }
-        }
-      ),
-
-    onColumnResize:
-      (gridName: string, columnIndex: number, newWidth: number) => dispatch(
-        resizeColumn({
-          name: gridName,
-          columnIndex,
-          newWidth
-        })
-      ),
-
-    onColumnMove:
-      (gridName: string, oldIndex: number, newIndex: number) => dispatch(
-        columnMove({
-          name: gridName,
-          oldIndex,
-          newIndex
-        })
-      ),
-
-    onSelectRow:
-      (rs: RecordSet, idx: number, selected: boolean) => dispatch(
-        selectRow({
-          name: rs.name,
-          idx,
-          selected
-        })
-      ),
-
-    onSelectAllRows:
-      (rs: RecordSet, value: boolean) => dispatch(
-        setAllRowsSelected({
-          name: rs.name,
-          value
-        })
-      ),
-
-    onSetCursorPos:
-      (rs: RecordSet, gridName: string, cursorCol: number, cursorRow: number) => {
-        dispatch(
-          setCurrentRow({
-            name: rs.name,
-            currentRow: cursorRow
-          })
-        );
-
-        dispatch(
-          setCursorCol({
-            name: gridName,
-            cursorCol
-          })
-        );
-      },
-
-    onSort:
-      (rs: RecordSet, sortFields: SortFields, gridRef?: GDMNGrid) => dispatch(
-        (dispatch: ThunkDispatch<IState, never, RecordSetAction>, getState: () => IState) => {
-          dispatch(
-            sortRecordSet({
-              name: rs.name,
-              sortFields
-            })
-          );
-
-          if (gridRef) {
-            gridRef.scrollIntoView(getState().recordSet[rs.name].currentRow);
-          }
-        }
-      ),
-
-    onToggleGroup:
-      (rs: RecordSet, rowIdx: number) => dispatch(
-        toggleGroup({
-          name: rs.name,
-          rowIdx
-        })
-      ),
-
+    ...connectDataViewDispatch(dispatch),
     loadFromERModel: (erModel: ERModel) => {
       const entitiesRS = RecordSet.createWithData(
         'entities',
