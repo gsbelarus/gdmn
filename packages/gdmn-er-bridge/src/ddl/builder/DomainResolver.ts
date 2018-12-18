4132848a6807aabdeff56037dcc8c9358f3cd02a
Lomashko Yuri <lomashko.yuri@gmail.com>
2018-12-18 17:21:05 +0300

    gdmn-er-bridge: use double precision instead float (#55)

--- packages/gdmn-er-bridge/src/ddl/builder/DomainResolver.ts
+++ packages/gdmn-er-bridge/src/ddl/builder/DomainResolver.ts
@@ -66,7 +66,7 @@ export class DomainResolver {
       case "TimeStamp":
         return `TIMESTAMP`;
       case "Float":
-        return `FLOAT`;
+        return `DOUBLE PRECISION`;
       case "Boolean":
         return `SMALLINT`;
       case "Blob":
