a18059f681e276f79953746d2ca210ecfc0903e0
Marina Karpukhina <marina.ks.developer@gmail.com>
2019-01-10 20:04:27 +0300

    gdmn-front: demo stress test

---
+++ src/gdmn-front/src/app/components/NumberTextField.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { ITextFieldProps, TextField } from 'office-ui-fabric-react';
+
+export interface INumberTextFieldProps extends ITextFieldProps {
+  label: string;
+  initialValue: string;
+}
+
+export interface INumberTextFieldState {
+  value: string;
+}
+
+export class NumberTextField extends React.Component<INumberTextFieldProps, INumberTextFieldState> {
+  constructor(props: INumberTextFieldProps) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+    this.validateNumber = this.validateNumber.bind(this);
+
+    this.state = {
+      value: props.initialValue
+    };
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="NumberTextField">
+        <TextField
+          className="NumberTextField-textField"
+          label={this.props.label}
+          value={this.state.value}
+          onChange={this.onChange}
+          onGetErrorMessage={this.validateNumber}
+        />
+      </div>
+    );
+  }
+
+  private validateNumber(value: string): string {
+    return isNaN(Number(value)) ? `The value should be a number, actual is ${value}.` : '';
+  }
+
+  private onChange(ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, value?: string): void {
+    return this.setState({
+      value: value || '0'
+    });
+  }
+
+}
--- src/gdmn-front/src/app/scenes/gdmn/components/StompDemoView.tsx
+++ src/gdmn-front/src/app/scenes/gdmn/components/StompDemoView.tsx
@@ -8,10 +8,15 @@ import { ICommand } from 'gdmn-nlp-agent/dist/definitions';
 import { TPingTaskCmd, TTaskActionNames } from '@gdmn/server-api';
 
 import { IViewProps, View } from '@src/app/components/View';
+import { NumberTextField } from '@src/app/components/NumberTextField';
+import { apiService } from '@src/app/services/apiService';
 
 interface IStompDemoViewState {
+  /* ping */
   pingDelay: string;
   pingSteps: string;
+  /* stress */
+  stressStarted: boolean;
 }
 
 interface IStompDemoViewProps extends IViewProps {
@@ -22,38 +27,189 @@ interface IStompDemoViewProps extends IViewProps {
 }
 
 class StompDemoView extends View<IStompDemoViewProps, IStompDemoViewState> {
+  initStressStepDuration: number = 2;
+  initStressStepInitRequestsCount: number = 10;
+  initStressStepIncRequestsCount: number = 10;
+
+  stressStepDurationFieldRef = React.createRef<NumberTextField>();
+  stressStepInitRequestsCountFieldRef = React.createRef<NumberTextField>();
+  stressStepIncRequestsCountFieldRef = React.createRef<NumberTextField>();
+
+  requestsCount: number = 0;
+  responseCount: number = 0;
+  stressIntervalId: any;
+
   public state: IStompDemoViewState = {
     pingDelay: '3000',
-    pingSteps: '2'
+    pingSteps: '2',
+    stressStarted: false
   };
 
   public getViewCaption(): string {
     return 'Stomp protocol';
   }
 
+  private handleStressApi = () => {
+    this.stressLoop(() => {
+      apiService
+        .ping({
+          payload: {
+            action: TTaskActionNames.PING,
+            payload: {
+              delay: 0,
+              steps: 0
+            }
+          }
+        })
+        .subscribe(value => {
+          this.responseCount++;
+        });
+    });
+  };
+
+  private handleStressDb = () => {
+    this.stressLoop(() => {
+      if (!this.props.erModel || Object.keys(this.props.erModel.entities).length === 0) return;
+
+      const entity = Object.values(this.props.erModel.entities)[0];
+      const query = new EntityQuery(
+        new EntityLink(
+          entity,
+          'alias',
+          Object.values(entity!.attributes)
+            .filter(value => value instanceof ScalarAttribute)
+            .map(value => new EntityQueryField(value))
+        )
+      );
+
+      apiService
+        .getData({
+          payload: {
+            action: TTaskActionNames.QUERY,
+            payload: query.inspect()
+          }
+        })
+        .subscribe(value => {
+          this.responseCount++;
+        });
+    });
+  };
+
+  private handleStopStress = () => {
+    this.setState({
+      stressStarted: false
+    });
+    clearInterval(this.stressIntervalId);
+  };
+
+  private stressLoop = (cb: Function) => {
+    this.setState(
+      {
+        stressStarted: true
+      },
+      () => {
+        let maxRequestsCount: number = parseInt(this.stressStepInitRequestsCountFieldRef.current!.state.value);
+        const incRequestsCount: number = parseInt(this.stressStepIncRequestsCountFieldRef.current!.state.value);
+        const stepDuration: number = parseInt(this.stressStepDurationFieldRef.current!.state.value);
+
+        this.stressIntervalId = window.setInterval((args: any) => {
+          console.log('setInterval');
+          if (this.responseCount < this.requestsCount)  {
+            console.log('setInterval: stop');
+            this.setState({
+              stressStarted: false
+            });
+
+            clearInterval(this.stressIntervalId);
+            this.stressIntervalId = null;
+          }
+        }, stepDuration * 100);
+
+
+        const f = () => {
+          if (this.requestsCount < maxRequestsCount) {
+            console.log('[test] do');
+            cb();
+
+            // todo: delay ?
+            this.requestsCount++;
+
+          } else if (this.responseCount >= this.requestsCount) {
+            console.log('[test] add step');
+            /* add step*/
+            maxRequestsCount += incRequestsCount;
+          }
+
+          if (this.state.stressStarted) {
+            setTimeout(() => {
+              f();
+            }, 0);
+          } else {
+            console.log('[test] clearInterval');
+            if (this.stressIntervalId) clearInterval(this.stressIntervalId);
+            this.responseCount = 0;
+            this.requestsCount = 0;
+            this.setState({
+              stressStarted: false
+            });
+          }
+        };
+
+        f();
+      }
+    );
+  };
+
   public render() {
     return this.renderOneColumn(
-      <div className="ViewBody" style={{ width: 'max-content' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <TextField
-            label="delay"
-            value={this.state.pingDelay.toString()}
-            onChange={e => this.handlePingDelayChange(e)}
-          />
-          <TextField
-            label="steps"
-            value={this.state.pingSteps.toString()}
-            onChange={e => this.handlePingStepsChange(e)}
-          />
-          <PrimaryButton onClick={this.handlePingClick} text="SEND PING-TASK" />
-          <br />
-          <PrimaryButton onClick={this.handleSendQueryClick} text="SEND QUERY-TASK" disabled={!this.props.erModel} />
-          <br />
-          <PrimaryButton
-            onClick={this.handleSendNlpQueryClick}
-            text="покажи все организации из минска и пинска"
-            disabled={!this.props.erModel}
-          />
+      <div>
+        <div className="ViewBody" style={{ width: 'max-content' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <TextField
+              label="delay"
+              value={this.state.pingDelay.toString()}
+              onChange={e => this.handlePingDelayChange(e)}
+            />
+            <TextField
+              label="steps"
+              value={this.state.pingSteps.toString()}
+              onChange={e => this.handlePingStepsChange(e)}
+            />
+            <PrimaryButton onClick={this.handlePingClick} text="SEND PING-TASK" />
+            <br />
+            <PrimaryButton onClick={this.handleSendQueryClick} text="SEND QUERY-TASK" disabled={!this.props.erModel} />
+            <br />
+            <PrimaryButton
+              onClick={this.handleSendNlpQueryClick}
+              text="покажи все организации из минска и пинска"
+              disabled={!this.props.erModel}
+            />
+          </div>
+        </div>
+        <div className="ViewBody" style={{ width: 'max-content', marginTop: 16 }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <NumberTextField
+              ref={this.stressStepDurationFieldRef}
+              initialValue={this.initStressStepDuration.toString()}
+              label="step duration (seconds)"
+            />
+            <NumberTextField
+              ref={this.stressStepInitRequestsCountFieldRef}
+              label="initial requests count"
+              initialValue={this.initStressStepInitRequestsCount.toString()}
+            />
+            <NumberTextField
+              ref={this.stressStepIncRequestsCountFieldRef}
+              label="increment requests count"
+              initialValue={this.initStressStepIncRequestsCount.toString()}
+            />
+            <br />
+            <PrimaryButton onClick={this.handleStressApi} text="STRESS API (PING-TASK)" />
+            <br />
+            <PrimaryButton onClick={this.handleStressDb} text="STRESS DB (QUERY-TASK)" disabled={!this.props.erModel} />
+            <br />
+            <PrimaryButton onClick={this.handleStopStress} text="STOP STRESS" />
+          </div>
         </div>
       </div>
     );
@@ -68,18 +224,6 @@ class StompDemoView extends View<IStompDemoViewProps, IStompDemoViewState> {
           steps: parseInt(this.state.pingSteps)
         }
       }
-    });
-  };
-
-  private handlePingDelayChange = (event: any) => {
-    this.setState({
-      pingDelay: event.target.value
-    });
-  };
-
-  private handlePingStepsChange = (event: any) => {
-    this.setState({
-      pingSteps: event.target.value
     });
   };
 
@@ -114,6 +258,20 @@ class StompDemoView extends View<IStompDemoViewProps, IStompDemoViewState> {
 
     cmds.forEach(value => {
       this.props.apiGetData(value.payload.inspect());
+    });
+  };
+
+  // todo refs
+
+  private handlePingDelayChange = (event: any) => {
+    this.setState({
+      pingDelay: event.target.value
+    });
+  };
+
+  private handlePingStepsChange = (event: any) => {
+    this.setState({
+      pingSteps: event.target.value
     });
   };
 }
