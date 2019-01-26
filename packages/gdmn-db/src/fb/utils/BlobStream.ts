ba181f3c1065a228cf0d38ef0e45fd278b66be99
Lomashko Yuri <lomashko.yuri@gmail.com>
2019-01-26 16:29:54 +0300

    gdmn-db: support for Blob::length for blobs greater than 2GB

--- packages/gdmn-db/src/fb/utils/BlobStream.ts
+++ packages/gdmn-db/src/fb/utils/BlobStream.ts
@@ -27,11 +27,12 @@ export class BlobStream {
             const infoRet = new Uint8Array(20);
             await this.handler!.getInfoAsync(status, infoReq.byteLength, infoReq, infoRet.byteLength, infoRet);
 
-            if (infoRet[0] !== blobInfo.totalLength || infoRet[1] !== 4 || infoRet[2] !== 0) {
+            if (infoRet[0] != blobInfo.totalLength) {
                 throw new Error("Unrecognized response from BlobObj::getInfo.");
             }
 
-            return getPortableInteger(infoRet.subarray(3), 4);
+            const size = getPortableInteger(infoRet.subarray(1), 2);
+            return getPortableInteger(infoRet.subarray(3), size);
         });
     }
 
