89ef1f1ebeb1002774c2ab9d8bd10575ddba96ba
Marina Karpukhina <marina.ks.developer@gmail.com>
2019-02-07 14:18:02 +0300

    gdmn-front: remove redux-persist reset workaround

--- src/gdmn-front/src/app/store/reducer.ts
+++ src/gdmn-front/src/app/store/reducer.ts
@@ -1,18 +1,18 @@
 import { combineReducers, Reducer } from 'redux';
 import { getType } from 'typesafe-actions';
-import { TActions } from '@src/app/store/TActions';
-import { _IAuthState, IAuthState, reducer as authReducer } from '@src/app/scenes/auth/reducer';
-import { IRootState, reducer as rootReducer } from '@src/app/scenes/root/reducer';
-import { reducer as gdmnReducer, TGdmnState } from '@src/app/scenes/gdmn/reducer';
-import { authActions } from '@src/app/scenes/auth/actions';
-import { recordSetReducer, RecordSetReducerState } from 'gdmn-recordset';
-import { gridReducer, GridReducerState } from 'gdmn-grid';
 // @ts-ignore
 import persistLocalStorage from 'redux-persist/lib/storage';
 // @ts-ignore
 import { PersistPartial, persistReducer } from 'redux-persist';
-
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+
+import { recordSetReducer, RecordSetReducerState } from 'gdmn-recordset';
+import { gridReducer, GridReducerState } from 'gdmn-grid';
+import { TActions } from '@src/app/store/TActions';
+import { IAuthState, reducer as authReducer } from '@src/app/scenes/auth/reducer';
+import { IRootState, reducer as rootReducer } from '@src/app/scenes/root/reducer';
+import { reducer as gdmnReducer, TGdmnState } from '@src/app/scenes/gdmn/reducer';
+import { authActions } from '@src/app/scenes/auth/actions';
 import { gdmnActions } from '@src/app/scenes/gdmn/actions';
 
 initializeIcons(/* optional base url */);
@@ -31,39 +31,25 @@ const authPersistConfig = {
   whitelist: ['authenticated', 'accessTokenPayload', 'refreshTokenPayload', 'accessToken', 'refreshToken']
 };
 
-const reducer = combineReducers<IState>({
-  rootState: rootReducer,
-  gdmnState: gdmnReducer,
-  authState: persistReducer(authPersistConfig, (state: _IAuthState | undefined, action: TActions) => {
-    /*
-     workaround for nested persist
-     see: https://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store
-    */
-    if (action.type === getType(authActions.onSignOut) || action.type === getType(gdmnActions.onApiDeleteAccount)) {
-      persistLocalStorage.removeItem(`persist:gdmn::root::authState`);
-    }
-
-    return authReducer(
-      action.type === getType(authActions.onSignOut) || action.type === getType(gdmnActions.onApiDeleteAccount)
-        ? undefined /* reset state to initial*/
-        : state,
-      action
-    );
-  }),
-  recordSet: recordSetReducer,
-  grid: gridReducer
-});
-
-const enhacedReducer = (state: IState | undefined, action: TActions) =>
-  reducer(
+function withReset(reducer: any) {
+  return (state: any, action: any) => reducer(
     action.type === getType(authActions.onSignOut) || action.type === getType(gdmnActions.onApiDeleteAccount)
       ? undefined /* reset state to initial*/
       : state,
     action
   );
+}
+
+const reducer = combineReducers<IState>({
+  rootState: withReset(rootReducer),
+  gdmnState: withReset(gdmnReducer),
+  authState: persistReducer(authPersistConfig, withReset(authReducer)),
+  recordSet: withReset(recordSetReducer),
+  grid: withReset(gridReducer)
+});
 
 type TReducer = Reducer<IState & PersistPartial, TActions>;
 
 // tslint:disable-next-line no-default-export
-export default enhacedReducer; // TODO test hmr require without default
+export default reducer; // TODO test hmr require without default
 export { TReducer, IState };
