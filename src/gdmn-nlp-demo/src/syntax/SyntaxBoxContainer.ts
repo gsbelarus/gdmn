3d0c0fa7d6a5b3abd862dcf153048e2f020d7bcc
andreik <gs1994@gmail.com>
2019-03-12 16:54:29 +0300

    gdmn-nlp-demo: clean up.

--- src/gdmn-nlp-demo/src/syntax/SyntaxBoxContainer.ts
+++ src/gdmn-nlp-demo/src/syntax/SyntaxBoxContainer.ts
@@ -81,8 +81,6 @@ export const SyntaxBoxContainer = connect(
             hideFooter: true
           }));
         } else {
-          console.log(query.inspect());
-
           let year = new Date().getFullYear();
           let month = new Date().getMonth() + 1;
           let day = new Date().getDate();
