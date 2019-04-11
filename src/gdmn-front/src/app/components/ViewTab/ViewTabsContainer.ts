f69355ebc9352d1f9031b27070c28ec851c1298a
Lomashko Yuri <lomashko.yuri@gmail.com>
2019-04-11 22:32:06 +0300

    gdmn-front: interrupt cursor task with closing tab (tmp)

--- src/gdmn-front/src/app/components/ViewTab/ViewTabsContainer.ts
+++ src/gdmn-front/src/app/components/ViewTab/ViewTabsContainer.ts
@@ -1,3 +1,4 @@
+import { apiService } from '@src/app/services/apiService';
 import { connect } from 'react-redux';
 import { IState, rsMetaActions } from '@src/app/store/reducer';
 import { IViewTab } from '@src/app/scenes/gdmn/types';
@@ -19,19 +20,19 @@ export const ViewTabsContainer = connect(
     ...stateProps,
     ...dispatchProps,
     onClose: async (vt: IViewTab) => {
-      const { history, location } = ownProps;
-      const { viewTabs, rsMeta, recordSet } = stateProps;
-      const { dispatch } = dispatchProps;
+      const {history, location} = ownProps;
+      const {viewTabs, rsMeta, recordSet} = stateProps;
+      const {dispatch} = dispatchProps;
 
-      const foundIdx = viewTabs.findIndex( t => t === vt );
+      const foundIdx = viewTabs.findIndex(t => t === vt);
 
       if (foundIdx === -1) return;
 
-      let nextPath = '';
+      let nextPath = "";
 
       if (location.pathname === vt.url) {
         if (viewTabs.length === 1) {
-          nextPath = '/spa/gdmn';
+          nextPath = "/spa/gdmn";
         } else {
           nextPath = foundIdx > 0 ? viewTabs[foundIdx - 1].url : viewTabs[foundIdx + 1].url;
         }
@@ -44,9 +45,17 @@ export const ViewTabsContainer = connect(
       dispatch(gdmnActions.deleteViewTab(vt));
 
       if (vt.rs) {
-        vt.rs
-          .filter(name => rsMeta[name])
-          .map(name => dispatch(rsMetaActions.deleteRsMeta(name)));
+        await Promise.all(
+          vt.rs
+            .filter(name => rsMeta[name])
+            .map(async name => {
+              const rsm = rsMeta[name];
+              dispatch(rsMetaActions.deleteRsMeta(name));
+              if (rsm.taskKey) {
+                await apiService.interruptTask({taskKey: rsm.taskKey});
+              }
+            })
+        );
         vt.rs
           .filter(rsName => recordSet[rsName])
           .forEach(rsName => dispatch(deleteRecordSet({name: rsName})));
