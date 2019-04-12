d04bb47c5e31f9f7a7a45fdfa006f075d6cb6be7
Stanislau Shliakhtsich <stasgm@gmail.com>
2019-04-12 20:08:42 +0300

    gdmn-internals: add types.ts

---
+++ packages/gdmn-internals/src/types.ts
@@ -0,0 +1,43 @@
+export enum Types {
+    BIGINT,
+    INTEGER,
+    SMALLINT,
+  
+    BLOB,
+    BOOLEAN,
+  
+    CHAR,
+    VARCHAR,
+  
+    DATE,
+    TIME,
+    TIMESTAMP,
+  
+    DOUBLE,
+    FLOAT,
+  
+    NULL,
+  
+    OTHER
+  }
+
+  export interface ISqlQueryResponseAliasesRdb {
+    type: Types;
+    label?: string;
+    field?: string;
+    relation?: string;
+  }
+  
+  export interface ISqlQueryResponseAliasesOrm {
+    type: string;
+    entity?: string;
+  }
+  
+  export interface ISqlQueryResponseAliases {
+    [alias: string]: {
+      rdb: ISqlQueryResponseAliasesRdb;
+      orm?: ISqlQueryResponseAliasesOrm;
+    }
+  }
+  
+  
