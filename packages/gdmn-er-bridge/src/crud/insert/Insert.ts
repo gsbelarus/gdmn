bee230667375dd892e223ab93d42c27b364d8eff
IharOknorp <pronko.im@gmail.com>
2019-06-20 12:35:03 +0300

    gdmn-er-bridge: fix SetAttr : Insert

--- packages/gdmn-er-bridge/src/crud/insert/Insert.ts
+++ packages/gdmn-er-bridge/src/crud/insert/Insert.ts
@@ -147,9 +147,9 @@ export class Insert {
         const set = field.value as TEntityInsertFieldSet;
         const mainCrossRelationName = AdapterUtils.getMainCrossRelationName(field.attribute);
         return set.map((entry) => {
-            const values: string[] = [];
             const mainCross: string[] = [];
             return entry.pkValues.map((pk) => {
+              const values: string[] = [];
               values.push(`${this._addToParams(pk)}`);
 
               entry.setAttributes && entry.setAttributes.map((s) => {
@@ -158,10 +158,10 @@ export class Insert {
               });
 
               return `\n  INSERT INTO` +
-                ` ${mainCrossRelationName}(${Constants.DEFAULT_CROSS_PK_OWN_NAME}, ` +
-                `${Constants.DEFAULT_CROSS_PK_REF_NAME}${mainCross.length ? ", " + mainCross.join(", ") : ""})` +
-                `\n  VALUES(:ID, ${values.join(", ")});\n`;
-            });
+                ` ${mainCrossRelationName}(${field.attribute.adapter.crossPk[0]}, ` +
+                `${field.attribute.adapter.crossPk[1]}${mainCross.length ? ", " + mainCross.join(", ") : ""})` +
+                `\n  VALUES(:ID, ${values.join(", ")});`;
+            }).join("\n")+"\n";
           }
         ).join("\n");
       }).join("\n");
