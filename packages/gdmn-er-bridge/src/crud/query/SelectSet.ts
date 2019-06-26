263d824e972c9065308842e2305ae0db9df24aa4
IharOknorp <pronko.im@gmail.com>
2019-06-26 16:05:50 +0300

    gdmn-er-bridge, gdmn-orm: fix SelectSet

--- packages/gdmn-er-bridge/src/crud/query/SelectSet.ts
+++ packages/gdmn-er-bridge/src/crud/query/SelectSet.ts
@@ -1,17 +1,14 @@
 import {
   Attribute,
-  EntityAttribute,
   EntityLink,
   EntityLinkField,
-  IEntityQueryAlias,
-  IRelation,
+  EntityQuerySet,
+  IEntityQuerySetWhere,
   ScalarAttribute,
-  SetAttribute,
-  EntityQuerySet, IEntityQuerySetWhere
+  SetAttribute
 } from "gdmn-orm";
 import {IParams} from "../..";
 import {AdapterUtils} from "../../AdapterUtils";
-import {Constants} from "../../ddl/Constants";
 import {SQLTemplates} from "./SQLTemplates";
 
 export interface IParams {
@@ -29,41 +26,21 @@ export class SelectSet {
 
   constructor(query: EntityQuerySet) {
     this.query = query;
-    this.sql = this._getSelect(this.query, true);
-  }
-
-  private static _arrayJoinWithBracket(array: string[], separator: string): string {
-    if (array.length === 1) {
-      return array.join(separator);
-    } else if (array.length > 1) {
-      return `(${array.join(separator)})`;
-    }
-    return "";
+    this.sql = this._getSelect(this.query);
   }
 
   private _findSetField(query: EntityQuerySet):EntityLinkField | undefined {
     return query.link.fields.find((field) => field.attribute.type === "Set")
   }
 
-  private _getSelect(query: EntityQuerySet, first?: boolean, withoutAlias?: boolean, existsLink?: EntityLink): string {
+  private _getSelect(query: EntityQuerySet, existsLink?: EntityLink): string {
     const {options, link} = query;
 
-    let sql = `SELECT`;
-
-    sql += `\n${this._makeFields(link, withoutAlias).join(",\n")}`;
-    sql += `\n${this._makeFrom(query, first)}`;
-
-    const sqlJoin = this._makeJoin(link).join("\n");
-    if (sqlJoin) {
-      sql += `\n${sqlJoin}`;
-    }
-
-    const sqlWhere = this._makeWhereLinkConditions(link)
-      .concat(this._makeWhereConditions(query, options && options.where, existsLink))
-      .join("\n  AND ");
-    if (sqlWhere) {
-      sql += `\nWHERE ${sqlWhere}`;
-    }
+    let sql = `SELECT` +
+      `\n${this._makeFields(link).join(",\n")}` +
+      `\n${this._makeFrom(query) + '\n'}` +
+      this._makeJoin(link).join("\n") +
+      `\nWHERE ${this._makeWhereConditions(query, options && options.where)}`;
     return sql;
   }
 
@@ -101,7 +78,7 @@ export class SelectSet {
     return fields.concat(joinedFields);
   }
 
-  private _makeFrom(query: EntityQuerySet, first?: boolean): string {
+  private _makeFrom(query: EntityQuerySet): string {
     const {link} = query;
     const findSetField =  this._findSetField(query);
 
@@ -112,96 +89,26 @@ export class SelectSet {
   }
 
   private _makeJoin(link: EntityLink): string[] {
-    const existsRelations = this._getExistsRelations(link);
-    if (!existsRelations.length) {
-      existsRelations.push(link.entity.adapter!.relation[0]);
-    }
-    const firstRelationName = existsRelations[0].relationName;
-    const firstPKFieldName = AdapterUtils.getPKFieldName(link.entity, firstRelationName);
 
     return link.fields.reduce((joins, field) => {
       if (field.links) {
         for (const fLink of field.links) {
-          const linkExistsRelations = this._getExistsRelations(fLink);
-          if (!linkExistsRelations.length) {
-            linkExistsRelations.push(fLink.entity.adapter!.relation[0]);
-          }
-          const linkFirstRelationName = linkExistsRelations[0].relationName;
-          const linkFirstPKFieldName = AdapterUtils.getPKFieldName(fLink.entity, linkFirstRelationName);
 
-          switch (field.attribute.type) {
-            case "Parent": {
-              const attr = field.attribute as EntityAttribute;
-              if (fLink.options && fLink.options.hasRoot) {
-                joins.push(
-                  SQLTemplates.joinWithTree(
-                    attr.adapter!.relation,
-                    this._getTableAlias(fLink, attr.adapter!.relation),
-                    Constants.DEFAULT_LB_NAME,
-                    this._getTableAlias(link, attr.adapter!.relation),
-                    Constants.DEFAULT_RB_NAME
-                  )
-                );
-              }
-              if (linkFirstRelationName !== attr.adapter!.relation || !(fLink.options
-                && fLink.options.hasRoot) && link.entity.isIntervalTree) {
+          const linkFirstRelationName = AdapterUtils.getMainRelationName(fLink.entity);
+          const linkFirstPKFieldName = AdapterUtils.getPKFieldName(fLink.entity, linkFirstRelationName);
+          if(field.attribute.type === "Set"){
+                const attr = field.attribute as SetAttribute;
                 joins.push(
                   SQLTemplates.join(
                     linkFirstRelationName,
                     this._getTableAlias(fLink, linkFirstRelationName),
                     linkFirstPKFieldName,
-                    this._getTableAlias(link, attr.adapter!.relation),
-                    attr.adapter!.field,
+                    this._getTableAlias(link, attr.adapter!.crossRelation),
+                    attr.adapter!.crossPk[1],
                     "LEFT"
                   )
                 );
-              }
-              break;
-            }
-            case "Set": {
-              const attr = field.attribute as SetAttribute;
-              joins.push(
-                SQLTemplates.join(
-                  linkFirstRelationName,
-                  this._getTableAlias(fLink, linkFirstRelationName),
-                  linkFirstPKFieldName,
-                  this._getTableAlias(link, attr.adapter!.crossRelation),
-                  attr.adapter!.crossPk[1],
-                  "LEFT"
-                )
-              );
-              break;
-            }
-            case "Entity":
-            default: {
-
-              const attr = field.attribute as EntityAttribute;
-              joins.push(
-                SQLTemplates.join(
-                  linkFirstRelationName,
-                  this._getTableAlias(fLink, linkFirstRelationName),
-                  linkFirstPKFieldName,
-                  this._getTableAlias(link, attr.adapter!.relation),
-                  attr.adapter!.field,
-                  "LEFT"
-                )
-              );
-            }
           }
-          linkExistsRelations.reduce((relJoins, rel) => {
-            if (fLink && linkFirstRelationName !== rel.relationName) {
-              relJoins.push(SQLTemplates.join(
-                rel.relationName,
-                this._getTableAlias(fLink, rel.relationName),
-                AdapterUtils.getPKFieldName(fLink.entity, rel.relationName),
-                this._getTableAlias(fLink, linkFirstRelationName),
-                AdapterUtils.getPKFieldName(fLink.entity, linkFirstRelationName),
-                "LEFT"
-              ));
-            }
-            return relJoins;
-          }, joins);
-          joins = joins.concat(this._makeJoin(fLink));
         }
         return joins;
       }
@@ -209,127 +116,18 @@ export class SelectSet {
     }, [] as string[]);
   }
 
-  private _makeWhereLinkConditions(link: EntityLink): string[] {
-    const whereEquals = link.entity.adapter!.relation
-      .filter((rel) => this._isExistsInLink(link, rel.relationName))
-      .reduce((equals, rel) => {
-        if (rel.selector) {
-          equals.push(
-            SQLTemplates.equals(
-              this._getTableAlias(link, rel.relationName),
-              rel.selector.field,
-              this._addToParams(rel.selector.value)
-            )
-          );
-        }
-        return equals;
-      }, [] as string[]);
-
-    return link.fields.reduce((equals, _field) => { // FIXME ???
-      // if (field.link) {
-      //   return equals.concat(this._makeWhereLinkConditions(field.link));
-      // }
-      return equals;
-    }, whereEquals);
-  }
-
-  private _makeWhereConditions(query: EntityQuerySet, where?: IEntityQuerySetWhere[], existsLink?: EntityLink): string[] {
+  private _makeWhereConditions(query: EntityQuerySet, where?: IEntityQuerySetWhere[]): string[] {
     if (!where) {
       return [];
     }
     const findSetField =  this._findSetField(query);
 
     return where.reduce((items, item) => {
-      const filters: string[] = [];
-
-      if (item.equals) {
-        const filterItems = item.equals.map((equals) => {
-          if (!equals.attribute) {
-            const alias = this._getTableAlias(query.link, findSetField!.attribute.adapter!.crossRelation);
-            return SQLTemplates.equals(alias, findSetField!.attribute.adapter!.crossPk[0], this._addToParams(equals.value));
-          }
-
-          const findLink = this._getLink(equals.alias, query.link);
-          const alias = this._getTableAlias(findLink, equals.attribute.adapter!.relation);
-          if (equals.value === Object(equals.value)) {
-            const value = (equals.value as IEntityQueryAlias<ScalarAttribute>);
-            const alias = this._getTableAlias(this._getLink(value.alias, query.link, existsLink),
-              value.attribute.adapter!.relation);
-            const field = `${alias && `${alias}.`}${value.attribute.name}`;
-
-            return SQLTemplates.equals(this._getTableAlias(this._getLink(equals.alias, query.link, existsLink),
-              value.attribute.adapter!.relation),
-               equals.attribute.adapter!.field,
-              field)
-          }
-          if (equals.attribute.type === "String") {
-            return SQLTemplates.equalsWithUpper(alias, equals.attribute.adapter!.field, this._addToParams(equals.value));
-          } else {
-            return SQLTemplates.equals(alias, equals.attribute.adapter!.field, this._addToParams(equals.value));
-          }
+        return item.equals!.map((equals) => {
+          const alias = this._getTableAlias(query.link, findSetField!.attribute.adapter!.crossRelation);
+          return SQLTemplates.equals(alias, findSetField!.attribute.adapter!.crossPk[0], this._addToParams(equals.value));
         });
-        const filter = SelectSet._arrayJoinWithBracket(filterItems, " AND ");
-        if (filter) {
-          filters.push(filter);
-        }
-      }
-
-      return items.concat(filters);
     }, [] as string[]);
-  }
-
-  private _isExistsInLink(link: EntityLink, relationName: string): boolean {
-    const existInFields = link.fields.some((field) => {
-      if (field.links) {
-        const attribute = field.attribute as EntityAttribute;
-        switch (attribute.type) {
-          case "Parent": {
-            if (attribute.adapter && attribute.adapter.relation === relationName) {
-              return true;
-            }
-            break;
-          }
-          case "Detail": {
-            const pkAttr = link.entity.pk[0];
-            if (pkAttr instanceof ScalarAttribute || pkAttr.type === "Entity") {
-              if (pkAttr.adapter.relation === relationName) {
-                return true;
-              }
-            }
-            break;
-          }
-          case "Set": {
-            const flag = field.setAttributes && field.setAttributes
-              .some((attr) => attr.adapter!.relation === relationName);
-            if (flag) {
-              return true;
-            }
-            break;
-          }
-          case "Entity":
-          default: {
-            if (attribute.adapter) {
-              return attribute.adapter.relation === relationName;
-            }
-            break;
-          }
-        }
-        return field.links.some((fLink) => this._isExistsInLink(fLink, relationName));
-      } else {
-        const attribute = field.attribute as ScalarAttribute;
-        return attribute.adapter!.relation === relationName;
-      }
-    });
-    if (existInFields) {
-      return true;
-    }
-
-    const where = this.query.options && this.query.options.where;
-    if (where) {
-        return true;
-    }
-
-    return false;
   }
 
   private _getLink(alias: string, link: EntityLink, existsLink?: EntityLink): EntityLink {
@@ -338,10 +136,6 @@ export class SelectSet {
       throw new Error(`Alias ${alias} is not found`);
     }
     return findLink;
-  }
-
-  private _getExistsRelations(link: EntityLink): IRelation[] {
-    return link.entity.adapter!.relation.filter((rel) => this._isExistsInLink(link, rel.relationName));
   }
 
   private _getTableAlias(link: EntityLink, relationName: string): string {
--- packages/gdmn-orm/src/query-models/EntityQuerySetOptions.ts
+++ packages/gdmn-orm/src/query-models/EntityQuerySetOptions.ts
@@ -6,18 +6,13 @@ import {ERModel} from "../model/ERModel";
 
 export interface IEntityQuerySetAliasInspector {
   alias: string;
-  attribute?: string;
 }
 
 export interface IEntityQuerySetWhereValueInspector extends IEntityQuerySetAliasInspector {
-  value: TValue | IEntityQuerySetAliasInspector;
+  value: TValue;
 }
 
 export interface IEntityQuerySetWhereInspector {
-  and?: IEntityQuerySetWhereInspector[];
-  or?: IEntityQuerySetWhereInspector[];
-  not?: IEntityQuerySetWhereInspector[];
-
   equals?: IEntityQuerySetWhereValueInspector[];
 }
 
@@ -27,17 +22,13 @@ export interface IEntityQuerySetOptionsInspector {
 
 export interface IEntityQuerySetAlias<Attr extends Attribute> {
   readonly alias: string;
-  readonly attribute?: Attr;
 }
 
 export interface IEntityQuerySetWhereValue extends IEntityQuerySetAlias<ScalarAttribute> {
-  readonly value: TValue | IEntityQuerySetAlias<ScalarAttribute>;
+  readonly value: TValue;
 }
 
 export interface IEntityQuerySetWhere {
-  readonly and?: IEntityQuerySetWhere[];
-  readonly or?: IEntityQuerySetWhere[];
-  readonly not?: IEntityQuerySetWhere[];
   readonly equals?: IEntityQuerySetWhereValue[];
 }
 
@@ -69,32 +60,14 @@ export class EntityQuerySetOptions {
                                         existlLink?: EntityLink): IEntityQuerySetWhere[] {
     return inspector.reduce((items, item) => {
       const where: IEntityQuerySetWhere = {
-        and: item.and ? EntityQuerySetOptions.inspectorWhereToObject(link, item.and) : undefined,
-        or: item.or ? EntityQuerySetOptions.inspectorWhereToObject(link, item.or) : undefined,
-        not: item.not ? EntityQuerySetOptions.inspectorWhereToObject(link, item.not) : undefined,
         equals: item.equals
           ? item.equals.map((equals) => {
             const findLink = link.deepFindLink(equals.alias);
             if (!findLink) {
               throw new Error(`Alias ${equals.alias} is not found`);
             }
-            if (!EntityQuerySetOptions._isValuePrimitiveInspector(equals.value)) {
-                const findLink2 = link.deepFindLink(equals.value.alias) ?
-                  link.deepFindLink(equals.value.alias) : existlLink && existlLink.deepFindLink(equals.value.alias);
-                if (!findLink2) {
-                  throw new Error(`Alias ${equals.value.alias} is not found`);
-                }
-                return {
-                alias: equals.alias,
-                attribute: equals.attribute ? findLink.entity.attribute(equals.attribute) : undefined,
-                value: {
-                  alias: equals.value.alias,
-                  attribute: equals.value.attribute ? findLink2.entity.attribute(equals.value.attribute) : undefined }
-              };
-            }
             return {
               alias: equals.alias,
-              attribute: equals.attribute ? findLink.entity.attribute(equals.attribute) : undefined,
               value: equals.value
             };
           })
@@ -110,30 +83,10 @@ export class EntityQuerySetOptions {
   private static _inspectWhere(where: IEntityQuerySetWhere[]): IEntityQuerySetWhereInspector[] {
     return where.reduce((items, item) => {
       const inspector: IEntityQuerySetWhereInspector = {};
-      if (item.and) {
-        inspector.and = EntityQuerySetOptions._inspectWhere(item.and);
-      }
-      if (item.or) {
-        inspector.or = EntityQuerySetOptions._inspectWhere(item.or);
-      }
-      if (item.not) {
-        inspector.not = EntityQuerySetOptions._inspectWhere(item.not);
-      }
       if (item.equals) {
         inspector.equals = item.equals.map((equals) => {
-          if (!EntityQuerySetOptions._isValuePrimitive(equals.value)) {
-            return {
-              alias: equals.alias,
-              attribute: equals.attribute ? equals.attribute.name : undefined,
-              value: {
-                  alias: equals.value.alias,
-                  attribute: equals.value.attribute ? equals.value.attribute.name : undefined
-                }
-            };
-          }
           return {
             alias: equals.alias,
-            attribute: equals.attribute ? equals.attribute.name : undefined,
             value: equals.value
           };
         });
@@ -142,16 +95,6 @@ export class EntityQuerySetOptions {
       items.push(inspector);
       return items;
     }, [] as IEntityQuerySetWhereInspector[]);
-  }
-
-  private static _isValuePrimitiveInspector(
-    value: TValue | IEntityQuerySetAliasInspector
-  ): value is TValue {
-    return value !== Object(value);
-  }
-
-  private static _isValuePrimitive(value: TValue | IEntityQuerySetAlias<ScalarAttribute>): value is TValue {
-    return value !== Object(value);
   }
 
   public inspect(): IEntityQuerySetOptionsInspector {
