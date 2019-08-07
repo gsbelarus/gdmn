91fe4aa00eb1321fb47ae1c1f2f9cef1b1314349
andreik <gs1994@gmail.com>
2019-08-07 14:39:47 +0300

    gdmn-front: switch tabs on internal styles.

--- src/gdmn-front/src/app/components/ViewTab/ViewTab.tsx
+++ src/gdmn-front/src/app/components/ViewTab/ViewTab.tsx
@@ -1,8 +1,7 @@
 import React, { Fragment } from 'react';
-import CSSModules from 'react-css-modules';
-import styles from './styles.css';
 import { Link } from 'react-router-dom';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react';
+import { useViewTabStyles } from './useViewTabStyles';
 
 export interface IViewTabProps {
   caption: string;
@@ -10,39 +9,39 @@ export interface IViewTabProps {
   error: boolean;
   loading: boolean;
   changed: boolean;
+  theme: string;
   onClose?: () => void;
 };
 
-export const ViewTab = CSSModules(
-  (props: IViewTabProps) => {
-    const { caption, url, loading, changed, error, onClose } = props;
-    const viewTabStyle = error ? 'ViewTab ViewTabError': 'ViewTab ViewTabNormal';
+export const ViewTab = (props: IViewTabProps) => {
+    const { caption, url, loading, changed, error, onClose, theme } = props;
+    const { viewTab, viewActiveColor, inactiveShadow, viewTabSpace, viewTabSpinner, viewTabCross, viewTabText, viewInactiveTab } = useViewTabStyles(theme, error);
 
-    const InnerTab = CSSModules( (props: {active?: boolean}) => (
+    const InnerTab = (props: {active?: boolean}) => (
       <Link to={url}>
-        <div styleName={`ViewTabText ${props.active ? "ViewActiveTab" : "ViewInactiveTab"}`}>
-          {loading ? <span styleName="ViewTabSpinner"><Spinner size={SpinnerSize.xSmall} /></span> : undefined}
+        <div style={props.active ? viewTabText : {...viewTabText, ...viewInactiveTab}}>
+          {loading ? <span style={viewTabSpinner}><Spinner size={SpinnerSize.xSmall} /></span> : undefined}
             {caption}{changed ? ' *' : ''}
-          {onClose && <span styleName="ViewTabCross" onClick={ e => { e.preventDefault(); onClose(); } }>x</span>}
+          {onClose && <span style={viewTabCross} onClick={ e => { e.preventDefault(); onClose(); } }>x</span>}
         </div>
       </Link>
-    ), styles, {allowMultiple: true });
+    );
 
     return url === location.pathname ? (
       <Fragment key={url}>
-        <div styleName={viewTabStyle} onMouseDown={event => event.button === 1 && onClose ? onClose() : undefined}>
-          <div styleName={error ? "ViewActiveColorError" : "ViewActiveColorNormal"} />
+        <div style={viewTab} onMouseDown={event => event.button === 1 && onClose ? onClose() : undefined}>
+          <div style={viewActiveColor} />
           <InnerTab active />
         </div>
-        <div styleName="ViewTabSpace" />
+        <div style={viewTabSpace} />
       </Fragment>
     ) : (
       <Fragment key={url}>
-        <div styleName={viewTabStyle} onMouseDown={event => event.button === 1 && onClose ? onClose() : undefined}>
+        <div style={viewTab} onMouseDown={event => event.button === 1 && onClose ? onClose() : undefined}>
           <InnerTab  />
-          <div styleName="ViewInactiveShadow" />
+          <div style={inactiveShadow} />
         </div>
-        <div styleName="ViewTabSpace" />
+        <div style={viewTabSpace} />
       </Fragment>
     );
-}, styles, { allowMultiple: true });
+};
---
+++ src/gdmn-front/src/app/components/ViewTab/useViewTabStyles.ts
@@ -0,0 +1,110 @@
+import { useEffect, useRef } from "react";
+import { getTheme } from "office-ui-fabric-react";
+
+interface IViewTabStyles {
+  viewTabsBand: React.CSSProperties;
+  viewTab: React.CSSProperties;
+  viewTabText: React.CSSProperties;
+  viewInactiveTab: React.CSSProperties;
+  viewActiveColor: React.CSSProperties;
+  inactiveShadow: React.CSSProperties;
+  viewTabSpace: React.CSSProperties;
+  viewRestSpace: React.CSSProperties;
+  viewTabSpinner: React.CSSProperties;
+  viewTabCross: React.CSSProperties;
+};
+
+export const useViewTabStyles = (theme: string, error?: boolean): IViewTabStyles => {
+  const styles = useRef<IViewTabStyles>();
+
+  useEffect( () => {
+    styles.current = {
+      viewTabsBand: {
+        width: '100%',
+        height: '36px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        paddingTop: '4px',
+        backgroundColor: getTheme().semanticColors.bodyBackground
+      },
+      viewTab: {
+        backgroundColor: error ? getTheme().palette.red : getTheme().semanticColors.bodyBackground,
+        color: error ? getTheme().palette.white : getTheme().semanticColors.bodyText,
+        minWidth: '96px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        cursor: 'default'
+      },
+      viewTabText: {
+        flex: '1 0 auto',
+        padding: '2px 4px 0px 4px',
+        textAlign: 'center',
+        minHeight: '27px',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        cursor: 'default',
+        borderLeft: '1px solid',
+        borderRight: '1px solid',
+        borderColor: getTheme().semanticColors.bodyText
+      },
+      viewInactiveTab: {
+        borderTop: '1px solid'
+      },
+      viewActiveColor: {
+        height: '5px',
+        borderLeft: '1px solid',
+        borderRight: '1px solid',
+        borderColor: getTheme().semanticColors.bodyText,
+        backgroundColor: error ? getTheme().palette.red : getTheme().palette.themeSecondary
+      },
+      inactiveShadow: {
+        borderColor: getTheme().semanticColors.bodyText,
+        backgroundColor: getTheme().semanticColors.bodyDivider,
+        height: '6px',
+        flex: '0 0 initial',
+        justifySelf: 'flex-end',
+        borderLeft: '1px solid',
+        borderRight: '1px solid',
+        borderBottom: '1px solid'
+      },
+      viewTabSpace: {
+        minWidth: '4px',
+        backgroundColor: 'transparent',
+        borderBottom: '1px solid',
+        borderColor: getTheme().semanticColors.bodyText,
+        flex: '0 0 initial'
+      },
+      viewRestSpace: {
+        backgroundColor: 'transparent',
+        borderBottom: '1px solid',
+        flex: '1 1 auto',
+        justifySelf: 'flex-end',
+        borderColor: getTheme().semanticColors.bodyText
+      },
+      viewTabSpinner: {
+        marginRight: '6px'
+      },
+      viewTabCross: {
+        marginLeft: '6px',
+        fontWeight: 'bold'
+      }
+    }
+  }, [theme, error]);
+
+  return styles.current || {
+    viewTabsBand: {},
+    viewTab: {},
+    viewTabText: {},
+    viewInactiveTab: {},
+    viewActiveColor: {},
+    inactiveShadow: {},
+    viewTabSpace: {},
+    viewRestSpace: {},
+    viewTabSpinner: {},
+    viewTabCross: {}
+  };
+};
