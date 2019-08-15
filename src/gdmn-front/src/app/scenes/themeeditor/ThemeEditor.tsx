20027897892180c9c2004a60ca4d98e03244e6a1
andreik <gs1994@gmail.com>
2019-08-15 09:59:46 +0300

    gdmn-front: added example on selecting items programatically.

--- src/gdmn-front/src/app/scenes/themeeditor/ThemeEditor.tsx
+++ src/gdmn-front/src/app/scenes/themeeditor/ThemeEditor.tsx
@@ -1,14 +1,30 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './style.css';
 import { IThemeEditorProps } from './ThemeEditor.types';
 import { gdmnActions } from '../gdmn/actions';
-import { ChoiceGroup, IChoiceGroupOption, Label, PrimaryButton, DefaultButton, createTheme, Customizer, loadTheme, getTheme } from 'office-ui-fabric-react';
+import { ChoiceGroup, IChoiceGroupOption, Label, PrimaryButton, DefaultButton, createTheme, Customizer, loadTheme, getTheme, DetailsList, IColumn, SelectionMode, DetailsListLayoutMode, Selection, MarqueeSelection } from 'office-ui-fabric-react';
 import { themes } from './themes';
 
 export const ThemeEditor = CSSModules( (props: IThemeEditorProps): JSX.Element => {
 
+  const getSelectionDetails = (): string => {
+    const selectionCount = selection.getSelection().length;
+    return selectionCount
+      ? `${selectionCount} items selected: ${selection.getSelection().map( (i: any) => i.key ).join('; ')}`
+      : 'No items selected';
+  }
+
+  const createSelection = () => {
+    return new Selection({
+      onSelectionChanged: () => setSelectionDetails(getSelectionDetails()),
+      getKey: (item: any) => item.key
+    });
+  };
+
   const { viewTab, dispatch, url, theme } = props;
+  const [selection, setSelection] = useState(createSelection());
+  const [selectionDetails, setSelectionDetails] = useState('');
 
   useEffect( () => {
     if (!viewTab) {
@@ -19,6 +35,45 @@ export const ThemeEditor = CSSModules( (props: IThemeEditorProps): JSX.Element =
       }));
     }
   }, []);
+
+  const items = [
+    {
+      key: '1',
+      value: 'sdksdfhdh'
+    },
+    {
+      key: '2',
+      value: '398457389457'
+    },
+    {
+      key: '3',
+      value: '65234jhsdgbv'
+    },
+    {
+      key: '4',
+      value: 'dlfjkkljgmn'
+    },
+    {
+      key: '5',
+      value: ';dldfmvkjn834'
+    },
+  ];
+
+  const columns: IColumn[] = [
+    {
+      key: 'column1',
+      name: 'Key',
+      minWidth: 64,
+      maxWidth: 96,
+      fieldName: 'key'
+    },
+    {
+      key: 'column2',
+      name: 'Value',
+      minWidth: 64,
+      fieldName: 'value'
+    }
+  ];
 
   return (
     <>
@@ -57,6 +112,36 @@ export const ThemeEditor = CSSModules( (props: IThemeEditorProps): JSX.Element =
       <Label>Examples</Label>
       <PrimaryButton>Primary button</PrimaryButton>
       <DefaultButton>Test</DefaultButton>
+      <div>
+        <Label>{selectionDetails}</Label>
+        <MarqueeSelection selection={selection}>
+          <DetailsList
+            items={items}
+            columns={columns}
+            selectionMode={SelectionMode.multiple}
+            getKey={ (item: any) => item.key }
+            setKey="set"
+            layoutMode={DetailsListLayoutMode.justified}
+            isHeaderVisible={true}
+            selection={selection}
+            selectionPreservedOnEmptyClick={true}
+            enterModalSelectionOnTouch={true}
+          />
+        </MarqueeSelection>
+        <DefaultButton
+          onClick={
+            () => {
+              selection.setItems(items);
+              for (let i = 1; i <= 3; i++) {
+                selection.setKeySelected(`${i}`, true, false);
+              }
+              setSelectionDetails(getSelectionDetails());
+            }
+          }
+        >
+          Set items selected
+        </DefaultButton>
+      </div>
     </>
   )
 }, styles, { allowMultiple: true });
