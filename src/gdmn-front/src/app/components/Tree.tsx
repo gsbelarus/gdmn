935d985aee10204de253d602173cefde49b0ccb3
ShiralievaSamira <sam.shiraliyeva@mail.ru>
2019-10-30 11:38:44 +0300

    gdmn-front: add parent to tree

--- src/gdmn-front/src/app/components/Tree.tsx
+++ src/gdmn-front/src/app/components/Tree.tsx
@@ -10,28 +10,34 @@ interface INode {
   rollUp?: boolean;
 }
 
-const Node = (props: {node: INode, level: (parent: string) => JSX.Element}) => {
+const Node = (props: {node: INode, level: (parent: string) => JSX.Element, isLast: boolean}) => {
   const [rollUp, setRollUp] = useState(props.node.rollUp ? props.node.rollUp : false);
 
-    return props.node.isLast
+    return props.isLast
             ? <div style={{marginLeft: '25px', cursor: 'pointer'}}>{props.node.value}</div>
             : (rollUp !== undefined && !rollUp)
-              ? <Stack horizontal styles={{root: {margin: '5px', cursor: 'pointer'}}}>
+              ? <Stack horizontal verticalAlign="center" styles={{root: {margin: '5px', cursor: 'pointer'}}}>
                   <Icon
-                      iconName="CaretSolidDown"
+                      iconName="ChevronRight"
                       onClick={() => setRollUp(!rollUp)}
+                      style={{
+                        fontSize: '10px'
+                      }}
                     />
-                  <div style={{marginLeft: '5px'}}>{props.node.value}</div>
+                  <div style={{ marginLeft: '5px'}}>{props.node.value}</div>
                 </Stack>
               : <Stack>
-                  <Stack horizontal styles={{root: {margin: '5px', cursor: 'pointer'}}}>
+                  <Stack horizontal verticalAlign="center" styles={{root: {margin: '5px', cursor: 'pointer'}}}>
                     <Icon
-                      iconName="CaretSolidUp"
+                      iconName="ChevronDown"
                       onClick={() => setRollUp(!rollUp)}
+                      style={{
+                        fontSize: '10px'
+                      }}
                     />
                     <div style={{marginLeft: '5px'}}>{props.node.value}</div>
                   </Stack>
-                  <div>{props.level(props.node.value)}</div>
+                  <div>{props.level(props.node.id)}</div>
                 </Stack>
 }
 
@@ -40,30 +46,39 @@ export const Tree = (props: {rs: RecordSet}) => {
   const nodes: INode[] = [{id: props.rs.name, value: props.rs.name, isLast: false}];
 
   const count = props.rs.size;
-  const f: INode[] = [];
 
   for(let i = 0; i < count; i++) {
-    //props.rs.getString('USR$NAME', i)
     const fdID = props.rs.params.fieldDefs.find(fd => fd.caption === 'ID')
     const fdNAME = props.rs.params.fieldDefs.find(fd => fd.caption === 'NAME')
     const fdUSRNAME = props.rs.params.fieldDefs.find(fd => fd.caption === 'USR$NAME')
+    const fdPARENT = props.rs.params.fieldDefs.find(fd => fd.caption === 'PARENT.ID')
+    const parent = fdPARENT ? props.rs.getString(fdPARENT.fieldName, i) : undefined;
     if(fdID && (fdNAME || fdUSRNAME)) {
       fdNAME
-        ? f.push({id: props.rs.getString(fdID.fieldName, i), value: props.rs.getString(fdNAME.fieldName, i), isLast: false} as INode)
+        ? nodes.push({
+            id: props.rs.getString(fdID.fieldName, i),
+            value: props.rs.getString(fdNAME.fieldName, i), 
+            parent: parent ? parent : props.rs.name,
+            isLast: false
+          } as INode)
         : fdUSRNAME
-          ? f.push({id: props.rs.getString(fdID.fieldName, i), value: props.rs.getString(fdUSRNAME.fieldName, i), isLast: false} as INode)
+          ? nodes.push({
+              id: props.rs.getString(fdID.fieldName, i), 
+              value: props.rs.getString(fdUSRNAME.fieldName, i), 
+              parent: parent ? parent : props.rs.name,
+              isLast: false
+            } as INode)
           : undefined
     }
   }
-
-  console.log(f)
-
+  
   const level = (parent: string) => {
     const nodeInLevel = nodes.filter( curr => curr.parent && curr.parent === parent)
     return <div style={{marginLeft: '25px', WebkitTouchCallout: 'none', WebkitUserSelect: 'none', KhtmlUserSelect: 'none', MozUserSelect: 'none', msUserSelect: 'none', userSelect: 'none'  }}>
       {
         nodeInLevel.map( node => {
-          return <Node key={`node-${node.value}-parent-${parent}`} node={node} level={level} />
+          const nextLevel: INode[] = nodes.filter( curr => curr.parent && curr.parent === node.id);
+          return <Node key={`node-${node.value}-parent-${parent}`} node={node} level={level} isLast={nextLevel.length === 0} />
         })
       }
     </div>
@@ -75,7 +90,7 @@ export const Tree = (props: {rs: RecordSet}) => {
     {
       roots !== []
       ? roots.map( (root, idx) => (
-        <Node key={`root-${idx}-node-${root.value}`} node={root} level={level} />
+        <Node key={`root-${idx}-node-${root.value}`} node={root} level={level} isLast={false} />
       ))
       : <div>Not found root</div>
     }
